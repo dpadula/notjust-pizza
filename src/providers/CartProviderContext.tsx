cbf648114766bc9160ebbb61a0f961f1ac7d9406
Diego <dpadula@github.com>
2025-12-06 20:20:57 -0300

    Agregando y evitando crear un item igual en el carro cuando es igual producto e igual tamaño. Solo se actualiza la cantidad

--- src/providers/CartProviderContext.tsx
+++ src/providers/CartProviderContext.tsx
@@ -5,7 +5,7 @@ import { createContext, PropsWithChildren, useContext, useState } from 'react';
 type CartContextType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem['size']) => void;
-  updateQuantity?: (itemId: string, delta: 1 | -1) => void;
+  updateQuantity: (itemId: string, delta: 1 | -1) => void;
 };
 
 const CartContext = createContext<CartContextType>({
@@ -18,6 +18,15 @@ const CartProvider = ({ children }: PropsWithChildren) => {
   const [items, setItems] = useState<CartItem[]>([]);
 
   const addItem = (product: Product, size: CartItem['size']) => {
+    const existingItem = items.find(
+      (item) => item.product_id === product.id && item.size === size
+    );
+    if (existingItem) {
+      updateQuantity(existingItem.id, 1);
+      return;
+    }
+    // If the item already exists in the cart with the same size, increase its quantity
+
     const newItem: CartItem = {
       id: randomUUID(),
       product,
@@ -29,12 +38,15 @@ const CartProvider = ({ children }: PropsWithChildren) => {
   };
 
   const updateQuantity = (itemId: string, delta: 1 | -1) => {
-    const updatedItems = items.map((item) =>
-      item.id !== itemId ? item : { ...item, quantity: item.quantity + delta }
-    );
+    const updatedItems = items
+      .map((item) =>
+        item.id !== itemId ? item : { ...item, quantity: item.quantity + delta }
+      )
+      .filter((item) => item.quantity > 0);
     setItems(updatedItems);
   };
-  const cart = { items, addItem }; // Aquí iría la lógica para manejar el carrito
+
+  const cart = { items, addItem, updateQuantity }; // Aquí iría la lógica para manejar el carrito
   return <CartContext.Provider value={cart}>{children}</CartContext.Provider>;
 };
 export const useCart = () => {
