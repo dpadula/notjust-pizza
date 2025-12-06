0155028840f8d8be31652e9ff316411c3714d91e
Diego <dpadula@github.com>
2025-12-06 16:48:05 -0300

    Acomodados varios estilos del detalle

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,17 +1,58 @@
+import products from '@assets/data/products';
+import { defaultPizzaImage } from '@components/ProductListItem';
+import Colors from '@constants/Colors';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const ProductDetailScreen = () => {
-  const { id } = useLocalSearchParams(); // Get the product ID from the route params
+  const { id } = useLocalSearchParams();
+
+  const product = products.find((p) => p.id.toString() === id);
+  if (!product) {
+    return (
+      <View>
+        <Text>Product not found</Text>
+      </View>
+    );
+  }
   return (
-    <View>
-      <Stack.Screen options={{ title: `Product Detail - ${id}` }} />
-      <Text>ProductDetailScreen {JSON.stringify(id)}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode='contain'
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
 
 export default ProductDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light.background,
+    padding: 10,
+    borderRadius: 20,
+  },
+
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: Colors.light.tint,
+  },
+});
--- src/app/(tabs)/menu/index.tsx
+++ src/app/(tabs)/menu/index.tsx
@@ -2,8 +2,6 @@ import products from '@assets/data/products';
 import ProductListItem from '@components/ProductListItem';
 import { FlatList, StyleSheet, View } from 'react-native';
 
-const product = products[0];
-
 export default function MenuScreen() {
   return (
     <View>
@@ -12,7 +10,7 @@ export default function MenuScreen() {
         renderItem={({ item }) => <ProductListItem product={item} />}
         numColumns={2}
         contentContainerStyle={{ gap: 10 }}
-        columnWrapperStyle={{ gap: 10  }}
+        columnWrapperStyle={{ gap: 10 }}
       />
     </View>
   );
