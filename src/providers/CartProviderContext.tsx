c205c410378970a7954f9ada57ac39cf6d1426ac
Diego <dpadula@github.com>
2025-12-06 20:09:02 -0300

    actualizando la cantidad

--- src/providers/CartProviderContext.tsx
+++ src/providers/CartProviderContext.tsx
@@ -5,11 +5,13 @@ import { createContext, PropsWithChildren, useContext, useState } from 'react';
 type CartContextType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem['size']) => void;
+  updateQuantity?: (itemId: string, delta: 1 | -1) => void;
 };
 
 const CartContext = createContext<CartContextType>({
   items: [],
   addItem: () => {},
+  updateQuantity: () => {},
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -24,6 +26,13 @@ const CartProvider = ({ children }: PropsWithChildren) => {
       quantity: 1,
     };
     setItems((prevItems) => [...prevItems, newItem]);
+  };
+
+  const updateQuantity = (itemId: string, delta: 1 | -1) => {
+    const updatedItems = items.map((item) =>
+      item.id !== itemId ? item : { ...item, quantity: item.quantity + delta }
+    );
+    setItems(updatedItems);
   };
   const cart = { items, addItem }; // Aquí iría la lógica para manejar el carrito
   return <CartContext.Provider value={cart}>{children}</CartContext.Provider>;
