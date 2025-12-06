ef4cc9c12525a5108f722576aa8c554128840e3b
Diego <dpadula@github.com>
2025-12-06 12:54:31 -0300

    Componente ProductListItem

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Colors from '../constants/Colors';
+
+type Product = {
+  product: {
+    id: number;
+    name: string;
+    image: string;
+    price: number;
+  };
+};
+
+const ProductListItem = ({ product }: Product) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.light.background,
+    padding: 10,
+    borderRadius: 20,
+  },
+
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: Colors.light.tint,
+  },
+});
