e1aa56cfaf84ffebfb933a3d7289dfabb24826e3
Diego <dpadula@github.com>
2025-12-06 13:18:14 -0300

    Usando Flatlist para renderizar los productos

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
 import products from '@assets/data/products';
 import ProductListItem from '@components/ProductListItem';
-import { StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 const product = products[0];
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+      />
     </View>
   );
 }
