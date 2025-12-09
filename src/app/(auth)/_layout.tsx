78455d55bd6d183984deffb04cce90c290a8fda5
Diego <dpadula@epe.santafe.gov.ar>
2025-12-09 10:26:07 -0300

    Agregado de layout para auth

---
+++ src/app/(auth)/_layout.tsx
@@ -0,0 +1,22 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/components/useColorScheme';
+
+export default function AuthLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name='sign-in' />
+        <Stack.Screen name='sign-up' />
+      </Stack>
+    </ThemeProvider>
+  );
+}
