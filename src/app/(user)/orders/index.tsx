a5d93170ba2b2179e76236f8d33ee603d0d2c22c
Diego <dpadula@epe.santafe.gov.ar>
2025-12-09 11:16:47 -0300

    Listado de ordenes y detalle de cada orden

--- src/app/(user)/orders/index.tsx
+++ src/app/(user)/orders/index.tsx
@@ -1,10 +1,12 @@
 import OrderListItem from '@/components/OrderListItem';
 import orders from '@assets/data/orders';
+import { Stack } from 'expo-router';
 import { FlatList, StyleSheet, View } from 'react-native';
 
 export default function OrdersScreen() {
   return (
     <View>
+      <Stack.Screen options={{ title: 'Orders' }} />
       <FlatList
         data={orders}
         renderItem={({ item }) => <OrderListItem order={item} />}
