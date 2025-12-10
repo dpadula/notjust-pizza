3904dd8ff33b2fa850a6f9dd67734659b3ae256f
Diego <dpadula@epe.santafe.gov.ar>
2025-12-10 13:16:47 -0300

    mecanismo usando tanstack query para traer de supa los productos

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,24 +1,53 @@
+import { supabase } from '@/lib/supabase';
 import products from '@assets/data/products';
 import ProductListItem from '@components/ProductListItem';
-import { useEffect } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
-import { supabase } from '../../../lib/supabase';
+import { useQuery } from '@tanstack/react-query';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 export default function MenuScreen() {
-  useEffect(() => {
-    const fetchProducts = async () => {
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     // Simulate a fetch call
+  //     console.log('Fetching products...');
+  //     const { data, error } = await supabase.from('products').select('*');
+  //     console.log('🚀 ~ fetchProducts ~ error:', error);
+  //     console.log('🚀 ~ fetchProducts ~ data:', data);
+  //   };
+
+  //   fetchProducts();
+  //   return () => {
+  //     console.log('MenuScreen unmounted');
+  //   };
+  // }, []);
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
       // Simulate a fetch call
       console.log('Fetching products...');
       const { data, error } = await supabase.from('products').select('*');
-      console.log('🚀 ~ fetchProducts ~ error:', error);
-      console.log('🚀 ~ fetchProducts ~ data:', data);
-    };
 
-    fetchProducts();
-    return () => {
-      console.log('MenuScreen unmounted');
-    };
-  }, []);
+      if (error) {
+        console.log('🚀 ~ fetchProducts ~ error:', error);
+        throw error;
+      }
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch products: {error.message}</Text>;
+  }
 
   return (
     <View>
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,8 +11,9 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import AuthProvider, { useAuth } from '@/providers/AuthProvider';
 import CartProvider from '@/providers/CartProviderContext';
-import AuthProvider, { useAuth } from '../providers/AuthProvider';
+import QueryProvider from '@/providers/QueryProvider';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -64,15 +65,17 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AuthProvider>
-        <CartProvider>
-          <Stack>
-            <Stack.Screen name='index' />
-            <Stack.Screen name='(user)' options={{ headerShown: false }} />
-            <Stack.Screen name='(admin)' options={{ headerShown: false }} />
-            <Stack.Screen name='(auth)' options={{ headerShown: false }} />
-            <Stack.Screen name='cart' options={{ presentation: 'modal' }} />
-          </Stack>
-        </CartProvider>
+        <QueryProvider>
+          <CartProvider>
+            <Stack>
+              <Stack.Screen name='index' />
+              <Stack.Screen name='(user)' options={{ headerShown: false }} />
+              <Stack.Screen name='(admin)' options={{ headerShown: false }} />
+              <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+              <Stack.Screen name='cart' options={{ presentation: 'modal' }} />
+            </Stack>
+          </CartProvider>
+        </QueryProvider>
       </AuthProvider>
     </ThemeProvider>
   );
