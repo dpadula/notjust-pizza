6d4d754a075207e8551458772e0b531e0af5f7e5
Diego <dpadula@epe.santafe.gov.ar>
2025-12-10 11:57:45 -0300

    Prueba de hacer fetch de productos despues de configurar datos y permisos en supa para la tabla productos

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,8 +1,25 @@
 import products from '@assets/data/products';
 import ProductListItem from '@components/ProductListItem';
+import { useEffect } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
+import { supabase } from '../../../lib/supabase';
 
 export default function MenuScreen() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      // Simulate a fetch call
+      console.log('Fetching products...');
+      const { data, error } = await supabase.from('products').select('*');
+      console.log('🚀 ~ fetchProducts ~ error:', error);
+      console.log('🚀 ~ fetchProducts ~ data:', data);
+    };
+
+    fetchProducts();
+    return () => {
+      console.log('MenuScreen unmounted');
+    };
+  }, []);
+
   return (
     <View>
       <FlatList
